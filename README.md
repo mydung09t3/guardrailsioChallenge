1fe842a6976715088866c5eb18468fc8a1bfd201
Dung Nguyen Thi My <mydung@inspectorio.com>
2022-11-07 21:16:35 +0700

    fix: update Readme file and delete method

--- README.md
+++ README.md
@@ -4,23 +4,32 @@ GuardRails Challenge
 1. Clone code from github
 2. Install nodes and npm package 
  If your machine already had Node and npm package, ignore this step
-3. run: npm install 
-  This step to install cypress, newman and dependencies   
-4. run command line: npm run test
-This one will run the test and generate report
-5. Go to folder cypress/reports/mochareports/report.html and copy the path then open in the browser to view report.
+3. Install cypress, newman and dependencies 
+   > npm install    
+4. Run the test and generate report 
+   > npm run test
+5. Go to folder "cypress/reports/mochareports/report.html" and copy the path then open in the browser to view report.
 6. How to run API Test: 
-   1. run command line: npm run apitest 
-   This command use to run api test. 
-   2. The API test store in cypress/test/API folder. It has 2 file: collection and enviroment.
+   1. Run below command line to run api test. 
+     > npm run apitest 
+   2. The API test is stored in "cypress/test/API folder". It has 2 files: collection.json and environment.json.
+   3. If you want to run in Postman please do below steps: 
+      1. Open Postman
+      2. Import collection.json file
+      3. Import environment.json file
+      4. Click on the collection "GuardRail Challenge" > select env is "guardrail" > Run collection
 # Explain packge.json file
 1. The run command is stored in package.json file
 2. To run test: 
- - npm run test  --this way use to run headless and generate report after run, test run in headless mode
- - npm run openTest -- this way use to open UI and view report directly. 
- - npx cypress run  - this way also use to run cypress
+ - "npm run test" : this way use to run headless and generate report after run, test run in headless mode
+ - "npm run openTest": this way use to open UI and view report directly. 
+ - "npm scripts": this way also use to run cypress but it will not gernerate report
  To run all test run all test but doesn't call mochawsome report config
- - npm run apitest: This way use to run the newman command to run api test suite.
-4. use: --browser chrome --into package.json "scripts" line if want to run on specific browser
-   - use "--headed" to force browser show when run cypress
-  ex: "scripts": "cypress run --browser chrome --headed" : it will run all test and show in browser. 
+ - "npm run apitest": This way use to run the newman command to run api test suite.
+4. Run headless and use specific browser:
+ - Add "--browser chrome" to after "cypress run" in "scripts" (package.json file) line if want to run on specific browser
+ - Add "--headed" to force browser show when run cypress
+  >ex: "scripts": "cypress run --browser chrome --headed" : it will run test using chrome and show the browser. 
+
+Note: I'm doing on MAC. If you run on Window, Please change the command "clean:reports" to like below:
+  > "clean:reports": "(if exist cypress\\reports (rmdir /S /Q cypress\\reports)) && mkdir cypress\\reports && mkdir cypress\\reports\\mocha  &&  mkdir cypress\\reports\\mochareports"
--- cypress/support/commands/commands.js
+++ cypress/support/commands/commands.js
@@ -28,94 +28,117 @@
  * Login Method
  * Param: email, password
  */
-Cypress.Commands.add('login', (email, passWord)=>{
-  cy.get('.mat-button-wrapper > span:contains(Account)').should("be.visible").click();
-  cy.get('#navbarLoginButton').should("be.visible").click();
-  cy.get("#email").should("be.visible").clear().type(email);
-  cy.get("#password").clear().type(passWord);
+Cypress.Commands.add('login', (email, passWord) => {
+  cy.get('.mat-button-wrapper > span:contains(Account)')
+    .should('be.visible')
+    .click();
+  cy.get('#navbarLoginButton').should('be.visible').click();
+  cy.get('#email').should('be.visible').clear().type(email);
+  cy.get('#password').clear().type(passWord);
   cy.get('#loginButton').click();
-} )
+});
 
 /**
  * Add Item to backset, Use this method to add a item to basket
  * param: item name
  */
-Cypress.Commands.add('addItemToBasket', (itemName)=>{
-  cy.get(`div:contains(${itemName})`).parent('div').parent('div').next().click().wait(2000);
-  cy.get('span.mat-simple-snack-bar-content').should("be.visible");
-})
+Cypress.Commands.add('addItemToBasket', itemName => {
+  cy.get(`div:contains(${itemName})`)
+    .parent('div')
+    .parent('div')
+    .next()
+    .click()
+    .wait(2000);
+  cy.get('span.mat-simple-snack-bar-content').should('be.visible');
+});
 
 /**
  * open Basket to view added item
  */
-Cypress.Commands.add('openBasket', ()=>{
+Cypress.Commands.add('openBasket', () => {
   cy.get('button[aria-label="Show the shopping cart"]').click();
-})
+});
 
 /**
  * Click checkout on the basket to continue order
  */
-Cypress.Commands.add('checkoutBasket', ()=>{
-  cy.get('#checkoutButton').should("be.visible").click();
-
-})
+Cypress.Commands.add('checkoutBasket', () => {
+  cy.get('#checkoutButton').should('be.visible').click();
+});
 
 /**
  * Click Add  new Address button
  */
-Cypress.Commands.add('clickAddNewAddress', ()=>{
-  cy.get('button[aria-label="Add a new address"]').should("be.visible").click();
-})
+Cypress.Commands.add('clickAddNewAddress', () => {
+  cy.get('button[aria-label="Add a new address"]').should('be.visible').click();
+});
 
 /**
  * fill address information
  * param: country, name, mobilePhone, zipCode, address, city, state
  */
-Cypress.Commands.add('fillAddress', (country, name, mobilePhone, zipCode, address, city, state)=>{
-  cy.get('input[data-placeholder="Please provide a country."]').type(country);
-  cy.get('input[data-placeholder="Please provide a name."]').type(name);
-  cy.get('input[data-placeholder="Please provide a mobile number."]').type(mobilePhone)
-  cy.get('input[data-placeholder="Please provide a ZIP code."]').type(zipCode);
-  cy.get('#address').type(address)
-  cy.get('input[data-placeholder="Please provide a city."]').type(city)
-  cy.get('input[data-placeholder="Please provide a state."]').type(state)
-  cy.get('#submitButton').click();
-})
+Cypress.Commands.add(
+  'fillAddress',
+  (country, name, mobilePhone, zipCode, address, city, state) => {
+    cy.get('input[data-placeholder="Please provide a country."]').type(country);
+    cy.get('input[data-placeholder="Please provide a name."]').type(name);
+    cy.get('input[data-placeholder="Please provide a mobile number."]').type(
+      mobilePhone,
+    );
+    cy.get('input[data-placeholder="Please provide a ZIP code."]').type(
+      zipCode,
+    );
+    cy.get('#address').type(address);
+    cy.get('input[data-placeholder="Please provide a city."]').type(city);
+    cy.get('input[data-placeholder="Please provide a state."]').type(state);
+    cy.get('#submitButton').click({ force: true });
+  },
+);
 
 /**
- * Delete all existing address 
+ * Delete all existing address
  */
-Cypress.Commands.add("deleteExistingAddress", ()=>{
+Cypress.Commands.add('deleteExistingAddress', () => {
   cy.get('span:contains(Account)').eq(1).click();
   cy.get('button[aria-label="Show Orders and Payment Menu"]').click();
   cy.get('button[aria-label="Go to saved address page"]').click();
-  cy.get('mat-table.mat-table').find('mat-row').its('length').then((length) =>{
-    cy.log(length);
-    for(let i =0; i<length; i++){
-      cy.log(i);
-      cy.get('[data-icon="trash-alt"]').eq(0).click().wait(1000);
-    }
-  })
-})
+  cy.get('mat-table.mat-table')
+    .find('mat-row')
+    .its('length')
+    .then(length => {
+      cy.log(length);
+      if (length > 0) {
+        for (let i = 0; i < length; i++) {
+          cy.log(i);
+          cy.get('[data-icon="trash-alt"]').eq(0).click().wait(1000);
+        }
+      }
+    });
+});
 
 /**
  * Search product by name
  * Param: productName
  */
-Cypress.Commands.add('searchProduct', (productName)=>{
+Cypress.Commands.add('searchProduct', productName => {
   cy.get('mat-icon.mat-search_icon-search').click();
   cy.get('mat-form-field.mat-search_field').type(`${productName}{enter}`);
-})
+});
 
 /**
  * Delete the added item in basket
  */
-Cypress.Commands.add("deleteAddedItem", ()=>{
-    cy.get('mat-table.mat-table').find('mat-row').its('length').then((length) =>{
+Cypress.Commands.add('deleteAddedItem', () => {
+  cy.get('mat-table.mat-table')
+    .find('mat-row')
+    .its('length')
+    .then(length => {
       cy.log(length);
-      for(let i =0; i<length; i++){
-        cy.log(i);
-        cy.get('[data-icon="trash-alt"]').eq(0).click().wait(1000);
+      if (length > 0) {
+        for (let i = 0; i < length; i++) {
+          cy.log(i);
+          cy.get('[data-icon="trash-alt"]').eq(0).click().wait(1000);
+        }
       }
-    })
-})
+    });
+});
